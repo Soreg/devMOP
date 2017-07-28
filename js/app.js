78b53f40f88c998f4cc4f73fa6f6a1e07380cd2e
Soren Gravesen <S_Gravesen92@hotmail.com>
2017-07-28 22:33:51 +0200

    Added test files to test merge conflicts

---
+++ js/app.js
@@ -0,0 +1,16 @@
+/*
+  Add respective JS here.
+  Please don't target default tags like "h2".
+  Instead, target defined tags, ex. "#feature1Heading"
+*/
+
+
+// Todo
+
+
+
+// Weather
+
+
+
+// Clock
