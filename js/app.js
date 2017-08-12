6705af59bd91f48d075a5a005277d3e7201218cc
mussol <gemma.valls@gmail.com>
2017-08-12 16:03:43 +0200

    fix a few styling issues

--- js/app.js
+++ js/app.js
@@ -179,7 +179,7 @@ $(document).ready(function() {
         randomArticle.link +
         ">" +
         randomArticle.article +
-        "</a>"
+        " <i class='fa fa-external-link' aria-hidden='true'></i></a>"
     );
   });
 });
