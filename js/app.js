72c68e20899680c0c03ae6e3c75c0dfa4b2c37cf
Soren Gravesen <S_Gravesen92@hotmail.com>
2017-09-08 17:23:46 +0200

    Images: Moved images to external source to reduce file size. Added Ral to allow external images.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,32 @@
 //================== Background images ==================
 $(document).ready(function() {
+
+  var imagesUrl = "http://www.sgcoding.media/devMOP-images/";
   var images = [
     '001.jpg','002.jpg','003.jpg','004.jpg','005.jpg','006.jpg','007.jpg','008.jpg','009.jpg','010.jpg',
     '011.jpg','012.jpg','013.jpg','014.jpg','015.jpg','016.jpg','017.jpg','018.jpg','019.jpg','020.jpg',
     '021.jpg','022.jpg','023.jpg','024.jpg','025.jpg','026.jpg','027.jpg','028.jpg','029.jpg','030.jpg',
     '031.jpg','032.jpg','033.jpg','034.jpg','035.jpg','036.jpg','037.jpg','038.jpg','039.jpg','040.jpg',
-    '041.jpg','042.jpg','043.jpg','044.jpg','045.jpg','046.jpg','047.jpg','048.jpg','049.jpg','050.jpg',
-    '051.jpg',
+    '041.jpg','042.jpg','043.jpg','044.jpg','045.jpg','046.jpg','047.jpg','048.jpg','049.jpg'
   ];
   var randomImage = images[Math.floor(Math.random()*images.length)];
+  var imagePath = imagesUrl + randomImage;
+
+  var remoteImage,
+    container = document.querySelector('.sidebar'),
+    toLoad = { 'images': [
+       imagePath, ] }; // list of image URLs
+
+  toLoad.images.forEach(function(imageToLoad) {
+        remoteImage = new RAL.RemoteImage(imageToLoad);
+        container.appendChild(remoteImage.element);
+        RAL.Queue.add(remoteImage);
+  });
   //display image
-  $('html').css('background-image', `url(/bg-images/${randomImage})`);
+  $('html').css('background-image', 'url(' + imagePath + ')');
 });
+RAL.Queue.setMaxConnections(4);
+RAL.Queue.start();
 
 //================== Greet user ==================
 var currentHour = new Date().getHours();
